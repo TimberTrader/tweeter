cc1b0e4916c77f0d3b85fd3a6307f9571c721f4e
vagrant <tp.bowes@gmail.com>
2019-02-20 14:25:20 +0000

    implemented char-count -fix turn counter back to default font.colour when coiunter goes from neg to pos

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $(".new-tweet textarea").keyup(function (e) { 
         let length = 140 - ($(this).val().length)
+        console.log(length);
         $(this).siblings(".counter").text(length);
-        if (length < 0) {
+        if (length >= 0) {
+            $(this).siblings(".counter");
+        } else {
             $(this).siblings(".counter").addClass("errColor");
         }
         e.preventDefault();
