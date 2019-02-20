320706924ceffd7a1d6453e29632f610906946c1
vagrant <tp.bowes@gmail.com>
2019-02-20 13:59:50 +0000

    1st cut at implementing char-counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".new-tweet textarea").keyup(function (e) { 
+        let length = 140 - ($(this).val().length)
+        $(this).siblings(".counter").text(length);
+        if (length < 0) {
+            $(this).siblings(".counter").addClass("errColor");
+        }
+        e.preventDefault();
+        });
+  });
+  
