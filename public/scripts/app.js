fa8f2ae42913a6484ede1b276aca237c95c5b796
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-02-21 09:12:36 -0700

    implemented dynamic tweets: craeteTweetElement  called by renderTweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,53 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * Client-side JS logic goes here  jQuery is already loaded
+ */
+ /*
+ fake data below
  */
 
+/$(document).ready(function() { 
+
+// post form for tweet
+const $form = $(aTweet)
+$form.on('submit', function(event) {
+    evt.preventDefault();
+    $.ajax({
+        url: '/tweets/',
+        data: $(this).serialize(),
+        method: 'POST',
+        success: loadTweets
+        },
+    })
+ })
+
+function loadTweets() {
+    $.ajax({
+        url: '/tweets/',
+        method: 'GET',
+        success: renderTweets
+    });
+}
+// takes dataObject and maps to the html template representing a tweet
+function createTweetElement(tweetData) {
+    return (
+     `<article class="tweet" action=/tweets>
+        <header>
+            <img class="avatar" src= ${tweetData.user.avatars.small}>
+            <h2 class="user">${tweetData.user.name}</h2>
+            <p class="handle">${tweetData.user.handle}</p>
+        </header>  
+        <div>${tweetData.content.text}</div> 
+            <footer>${tweetData.created_at}</footer>
+        </article>`
+        )};
+   
+function renderTweets(tweet) {
+    console.log('tweet', tweet)
+    tweet.forEach(tweet => {
+        let twtHTML = createTweetElement(tweet);
+    $("#tweets-container").prepend(twtHTML)
+    });
+  };
+// })
+
+renderTweets();
